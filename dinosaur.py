77f19daf4ade87f7143ebbb1698f64b9590bc04e
Jimmy Adediran <jimmy@Jimmys-MacBook-Pro.local>
2022-05-21 17:29:15 -0400

    Dinosaur class createdin dinosaur.py

---
+++ dinosaur.py
@@ -0,0 +1,8 @@
+class Dinosaur:
+    def __init__(self, name, attack_power):
+        self.name = name
+        self.attack_power = attack_power
+        self.health = ""
+    def attack(self, robot):
+        attack_robot = robot
+        pass
