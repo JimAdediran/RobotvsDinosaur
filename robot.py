42f98e2e3590e08f86337a35e7c6c055a56b2f2b
Jimmy Adediran <jimmy@Jimmys-MacBook-Pro.local>
2022-05-21 17:36:06 -0400

    Robot class created in robot.py

---
+++ robot.py
@@ -0,0 +1,10 @@
+from weapon import Weapon
+
+class Robot:
+    def __init__(self, name):
+        self.name = name
+        self.health = ""
+        self.active_weapon = Weapon
+    def attack(self, dinosaur):
+        attack = dinosaur
+        pass
