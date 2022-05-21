d50349aed999fb8a308e0067835ab6db0ab34b6b
Jimmy Adediran <jimmy@Jimmys-MacBook-Pro.local>
2022-05-21 19:18:37 -0400

    robot and dinosaur given names for the parameter

--- battlefield.py
+++ battlefield.py
@@ -4,32 +4,33 @@ from weapon import Weapon
 
 class Battlefield:
     def __init__(self):
-        self.robot = Robot
-        self.dinosaur = Dinosaur
-    def run_game(self):
-        def display_welcome():
-            def battle_phase():
-                def display_winner():
-                    pass
+        self.robot = Robot(John)
+        self.dinosaur = Dinosaur(Kyle)
 
-    def display_welcome(self):
-        print("Welcome ot an epic battle for the ages!")
-        print("Only one side can win!")
-        pass
-    def battle_phase(self):
-        while Robot.health > 0 and Dinosaur.heath > 0:
-            Robot.attack
-            print(f'{Robot} attacked {Dinosaur} for {Weapon.attack_power} damage!')
-            print(f'{Dinosaur} has {Dinosaur.health} remaining!')
-            Dinosaur.attack
-            print(f'{Dinosaur} attacked {Robot} for {Dinosaur.attack_power} damage!')
-            print(f'{Robot} has {Robot.health} remaining!')
+def run_game(self):
+            def display_welcome():
+                def battle_phase():
+                    def display_winner():
+                        pass
 
-    def display_winner(self):
-        if Robot.health <= 0:
-            print(f'{Dinosaur} made {Robot} extinct!')
-            print(f'{Dinosaur} wins!')
-        else:
-            print(f'{Robot} made {Dinosaur} extinct!')
-            print(f'{Robot} wins!')
+def display_welcome(self):
+    print("Welcome ot an epic battle for the ages!")
+    print("Only one side can win!")
+    pass
+def battle_phase(self):
+            while Robot.health > 0 and Dinosaur.heath > 0:
+                Robot.attack
+                print(f'{Robot} attacked {Dinosaur} for {Weapon.attack_power} damage!')
+                print(f'{Dinosaur} has {Dinosaur.health} remaining!')
+                Dinosaur.attack
+                print(f'{Dinosaur} attacked {Robot} for {Dinosaur.attack_power} damage!')
+                print(f'{Robot} has {Robot.health} remaining!')
+
+def display_winner(self):
+    if Robot.health <= 0:
+        print(f'{Dinosaur} made {Robot} extinct!')
+        print(f'{Dinosaur} wins!')
+    else:
+        print(f'{Robot} made {Dinosaur} extinct!')
+        print(f'{Robot} wins!')
         pass
