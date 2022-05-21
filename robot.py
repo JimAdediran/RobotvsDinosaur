c686c4d5aa96a8e0f29b3a5f493ed86904ab8d56
Jimmy Adediran <jimmy@Jimmys-MacBook-Pro.local>
2022-05-21 17:58:14 -0400

    Robot health changed from empty string to 100

--- robot.py
+++ robot.py
@@ -3,7 +3,7 @@ from weapon import Weapon
 class Robot:
     def __init__(self, name):
         self.name = name
-        self.health = ""
+        self.health = 100
         self.active_weapon = Weapon
     def attack(self, dinosaur):
         attack = dinosaur
