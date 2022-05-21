f1c39c03b10d1d2d4382c7c4466c494198d84a9c
Jimmy Adediran <jimmy@Jimmys-MacBook-Pro.local>
2022-05-21 18:55:16 -0400

    updated to include logic for the functions to perform the game in main.py

--- battlefield.py
+++ battlefield.py
@@ -1,17 +1,35 @@
 from robot import Robot
 from dinosaur import Dinosaur
+from weapon import Weapon
 
 class Battlefield:
     def __init__(self):
         self.robot = Robot
         self.dinosaur = Dinosaur
     def run_game(self):
-        pass
+        def display_welcome():
+            def battle_phase():
+                def display_winner():
+                    pass
+
     def display_welcome(self):
+        print("Welcome ot an epic battle for the ages!")
+        print("Only one side can win!")
         pass
     def battle_phase(self):
-        pass
+        while Robot.health > 0 and Dinosaur.heath > 0:
+            Robot.attack
+            print(f'{Robot} attacked {Dinosaur} for {Weapon.attack_power} damage!')
+            print(f'{Dinosaur} has {Dinosaur.health} remaining!')
+            Dinosaur.attack
+            print(f'{Dinosaur} attacked {Robot} for {Dinosaur.attack_power} damage!')
+            print(f'{Robot} has {Robot.health} remaining!')
+
     def display_winner(self):
-        pass
-    
+        if Robot.health <= 0:
+            print(f'{Dinosaur} made {Robot} extinct!')
+            print(f'{Dinosaur} wins!')
+        else:
+            print(f'{Robot} made {Dinosaur} extinct!')
+            print(f'{Robot} wins!')
         pass
